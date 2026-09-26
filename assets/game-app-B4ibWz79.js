cf7f65dac450d895de897b44f842be14af9b42a4
RYOLEE <ecolu.o.ryosuke@gmail.com>
2026-09-27 04:12:41 +0900

    Publish the visible walls trial

--- assets/game-app-B4ibWz79.js
+++ assets/game-app-B4ibWz79.js
@@ -1,4 +1,4 @@
-import{n as e,r as t,t as n}from"./index-XHyVM0Ae.js";import{_ as r,a as i,c as a,d as o,i as s,l as c,m as l,n as u,o as d,s as f,t as p}from"./rotate-ccw-BaK8CUJf.js";import{n as m,t as h}from"./play-Cv-16Bkt.js";import{n as g,t as _}from"./volume-x-g0XdRXKu.js";var v=t(e(),1),y=64,b=`
+import{n as e,r as t,t as n}from"./index-DrI_A_U9.js";import{_ as r,a as i,c as a,d as o,i as s,l as c,m as l,n as u,o as d,s as f,t as p}from"./rotate-ccw-fSrs8ANw.js";import{n as m,t as h}from"./play-CAbXSDpa.js";import{n as g,t as _}from"./volume-x-CJZDg6wi.js";var v=t(e(),1),y=64,b=`
 attribute vec2 a_pos;
 void main() {
   gl_Position = vec4(a_pos, 0.0, 1.0);
